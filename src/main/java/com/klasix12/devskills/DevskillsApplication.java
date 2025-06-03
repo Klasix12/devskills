e37fb87ea27798a7d3c9a904d235ac6136267ece
klasix12 <sunlightpattern@yandex.ru>
2025-06-03 21:06:10 +0300

    init commit

---
+++ src/main/java/com/klasix12/devskills/DevskillsApplication.java
@@ -0,0 +1,13 @@
+package com.klasix12.devskills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevskillsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevskillsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klasix12/devskills/DevskillsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klasix12.devskills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevskillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

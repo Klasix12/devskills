0de263cd61e14b46d32f42b53c068a8ebd05c34c
klasix12 <sunlightpattern@yandex.ru>
2025-06-05 00:48:22 +0300

    test: add UserService createUser test

--- src/main/java/com/klasix12/devskills/dto/UserRegistrationRequest.java
+++ src/main/java/com/klasix12/devskills/dto/UserRegistrationRequest.java
@@ -4,11 +4,13 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
 @Schema(description = "Request object for user registration")
 public class UserRegistrationRequest {
 

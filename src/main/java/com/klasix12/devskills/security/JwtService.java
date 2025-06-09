e7b3ca8964b389e59ce5209d76646932c5651482
klasix12 <sunlightpattern@yandex.ru>
2025-06-09 12:43:51 +0300

    chore: move JwtService from service to security package

--- src/main/java/com/klasix12/devskills/security/JwtService.java
+++ src/main/java/com/klasix12/devskills/security/JwtService.java
@@ -1,8 +1,7 @@
-package com.klasix12.devskills.service;
+package com.klasix12.devskills.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
